deadff25bf2ad10a0eeb42bcebbffaeb343f2c74
stathis21098 <mrkaboom14@gmail.com>
2018-03-26 02:32:20 +0300

    First Commit

---
+++ include/Monster.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iostream>
+
+class Monster {
+  private:
+    // Info
+    std::string monster_name;
+    int level;
+    std::string category;
+    std::string rarity;
+    // Stats
+    int health_points;
+    int defence;
+    int attack_damage;
+    int attack_speed;
+    int critical_chance;
+    int magic_damage;
+    int charges;
+  public:
+    // Getters
+    // Info
+    std::string get_monster_name();
+    int get_level();
+    std::string get_category();
+    std::string get_rarity();
+    // Stats
+    int get_health_points();
+    int get_defence();
+    int get_attack_damage();
+    int get_attack_speed();
+    int get_critical_chance();
+    int get_magic_damage();
+    int get_charges();
+
+    // Setters
+    // Info
+    void set_monster_name(std::string);
+    void set_level(int);
+    void set_category(std::string);
+    void set_rarity(std::string);
+    // Stats
+    void set_health_points(int);
+    void set_defence(int);
+    void set_attack_damage(int);
+    void set_attack_speed(int);
+    void set_critical_chance(int);
+    void set_magic_damage(int);
+    void set_charges(int);
+};
---
+++ src/Monster.cpp
@@ -0,0 +1,93 @@
+#include "Monster.h"
+
+// Getters
+// Info
+std::string Monster::get_monster_name() {
+  return this->monster_name;
+}
+
+int Monster::get_level() {
+  return this->level;
+}
+
+std::string Monster::get_category() {
+  return this->category;
+}
+
+std::string Monster::get_rarity() {
+  return this->rarity;
+}
+// Stats
+int Monster::get_health_points() {
+  return this->health_points;
+}
+
+int Monster::get_defence() {
+  return this->defence;
+}
+
+int Monster::get_attack_damage() {
+  return this->attack_damage;
+}
+
+int Monster::get_attack_speed() {
+  return this->attack_speed;
+}
+
+int Monster::get_critical_chance() {
+  return this->critical_chance;
+}
+
+int Monster::get_magic_damage() {
+  return this->magic_damage;
+}
+
+int Monster::get_charges() {
+  return this->charges;
+}
+
+// Setters
+// Info
+void Monster::set_monster_name(std::string monster_name) {
+  this->monster_name = monster_name;
+}
+
+void Monster::set_level(int level) {
+  this->level = level;
+}
+
+void Monster::set_category(std::string category) {
+  this->category = category;
+}
+
+void Monster::set_rarity(std::string rarity) {
+  this->rarity = rarity;
+}
+// Stats
+void Monster::set_health_points(int health_points) {
+  this->health_points = health_points;
+}
+
+void Monster::set_defence(int defence) {
+  this->defence = defence;
+}
+
+void Monster::set_attack_damage(int attack_damage) {
+  this->attack_damage = attack_damage;
+}
+
+void Monster::set_attack_speed(int attack_speed) {
+  this->attack_speed = attack_speed;
+}
+
+void Monster::set_critical_chance(int critical_chance) {
+  this->critical_chance = critical_chance;
+}
+
+void Monster::set_magic_damage(int magic_damage) {
+  this->magic_damage = magic_damage;
+}
+
+void Monster::set_charges(int charges) {
+  this->charges = charges;
+}
